387c53cbcfc9deeda61b10e03094575a83b83cce
Paulina Salem <paulinasalem@yahoo.com>
2018-04-17 23:10:37 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ if array.length == 1
 elsif array.length == 2
   array.join(" and ")
 else
-  array.pop = last_one
+  last_one = array.pop
   last_one.unshift('and ')
   array.join(' , ')
   array.push(last_one)
