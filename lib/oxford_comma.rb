eaa44e908e94ec3835695703fbee76291f25a01a
Paulina Salem <paulinasalem@yahoo.com>
2018-04-17 23:04:21 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,13 @@ if array.length == 1
 
 elsif array.length == 2
   array.join(" and ")
-elsif array.length == 3
-  array_new = []
-array_new.push(array.insert(2, 'and'))
-array_new.join(", ")
+else 
+  last_one = []
+  last_one.push(array.pop)
+  last_one.unshift('and')
+#elsif array.length == 3
+#  array_new = []
+#array_new.push(array.insert(2, 'and'))
+#array_new.join(", ")
 end
 end
