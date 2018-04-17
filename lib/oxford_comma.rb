af341d6d4e29fd0c67c3adb9cb426cec162a9a1d
Paulina Salem <paulinasalem@yahoo.com>
2018-04-17 23:17:57 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+if array.length == 1
+  array.join
 
+elsif array.length == 2
+  array.join(" and ")
+else
+  last_one = array.pop
+  new_string = array.join(', ')
+  new_string+(', and ')+ (last_one)
+end
 end
